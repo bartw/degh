91406f26319127e897a464b490765362b7eeddf6
Bart Wijnants <bartwijnants@outlook.com>
2017-06-01 22:13:29 +0200

    commits

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,19 +2,48 @@ import React from 'react';
 import { gql, graphql } from 'react-apollo';
 
 function App({loading, repository}) {
+  const commits = loading ? [] : repository.ref.target.history.edges.map(edge => <li key={edge.node.oid}>{edge.node.messageHeadline}</li>);
   return (
     <div>
       {loading && <div>loading</div>}
-      {!loading && <div>Hello {repository.name}, you were created at {repository.createdAt}</div>}
+      {
+        !loading && 
+        <div>
+          <h1>Hello {repository.name}, you were created at {repository.createdAt}</h1>
+          <h2>Commits</h2>
+          <ul>
+            {commits}
+          </ul>
+        </div>
+      }
     </div>
   );
 }
 
 const repoQuery = gql`
   query Repo {
-    repository(owner:"bartw", name:"degh") {
-      name,
+    repository(owner: "bartw", name: "degh") {
+      name
       createdAt
+      ref(qualifiedName: "master") {
+        target {
+          ... on Commit {
+            id
+            history(first: 10) {
+              edges {
+                node {
+                  messageHeadline
+                  oid
+                  author {
+                    name
+                    date
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
     }
   }
 `;
